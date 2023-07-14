95760d300ec593930d5d58dafa10e7c758b08e01
BBMinhh <136958412+BBMinhh@users.noreply.github.com>
2023-07-14 12:38:00 +0700

    Add files via upload

---
+++ bai3.js
@@ -0,0 +1,11 @@
+function bai3() {
+    let story = prompt("Em đã ăn cơm chưa?", "Yes/No");
+    if (story === "Yes") {
+      document.getElementById("demo").innerHTML =
+      story = prompt("Em có muốn ăn tráng miệng không ?", "Yes/No");
+    }
+    if (story == "No") {
+        document.getElementById("demo").innerHTML =
+        story = prompt("Em có muốn ăn cơm với sườn xào chua ngọt không ?", "Yes/No");
+    }
+  }
---
+++ main.js
@@ -0,0 +1,7 @@
+let myArray=[9,7,9,0,8]
+
+      for (value of myArray) {
+         if(value%2 == 0){
+            console.log(value);
+         }
+    }
